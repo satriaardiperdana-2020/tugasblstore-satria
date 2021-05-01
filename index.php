d9d774e4e570b6f3cd7a5ef98c9cf16801ed5b36
Satria Ardi Perdana <55689358+satriaardiperdana-2020@users.noreply.github.com>
2021-05-01 23:57:32 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -21,9 +21,9 @@
 		</td>
 		<td width="125">
 			<select name="size1">
-				<option value="satuTB"> 1TB</option>
-				<option value="duaTB">  2TB</option>
-				<option value="empatTB">4TB</option>
+				<option value="1"> 1TB</option>
+				<option value="2">  2TB</option>
+				<option value="4">4TB</option>
 			</select>
 		</td>
 		<td width="125">
@@ -46,9 +46,9 @@
 		</td>
 		<td width="125">
 			<select name="size2">
-				<option value="satuTB"> 1TB</option>
-				<option value="duaTB">  2TB</option>
-				<option value="empatTB">4TB</option>
+				<option value="1"> 1TB</option>
+				<option value="2">  2TB</option>
+				<option value="4">4TB</option>
 			</select>
 		</td>
 		<td width="125">
@@ -71,9 +71,9 @@
 		</td>
 		<td width="125">
 			<select name="size3">
-				<option value="satuTB"> 1TB</option>
-				<option value="duaTB">  2TB</option>
-				<option value="empatTB">4TB</option>
+				<option value="1"> 1TB</option>
+				<option value="2">  2TB</option>
+				<option value="4">4TB</option>
 			</select>
 		</td>
 		<td width="125">
@@ -106,7 +106,9 @@
 	$dsc1=0;
 	$dsc2=0;
 	$dsc3=0;
-	$price=0;
+	$price1=0;
+	$price2=0;
+	$price3=0;
 	$pajak=0;
 	$total=0;
 	$subtotal1=0;
@@ -130,126 +132,149 @@
 		$dsc3 = $_POST['dsc3'];
 
 		if ($item1=='toshiba' && !empty($qty1)) {
-			if ($size1=='satuTB') {
-				$price=52;
-			}elseif ($size1=='duaTB') {
-				$price=75;
-			}elseif ($size1=='empatTB') {
-				$price=150;
+			if ($size1=='1') {
+				$price1=52;
+			}elseif ($size1=='2') {
+				$price1=75;
+			}elseif ($size1=='4') {
+				$price1=150;
 			}
-			$subtotal1=($price*$qty1)-(($dsc1/100)*($price*$qty1));
-			echo $price;
-			echo $qty1;
-			echo $dsc1;
-			echo $subtotal1;			
+			$subtotal1=($price1*$qty1)-(($dsc1/100)*($price1*$qty1));			
 		}elseif($item1=='samsung' && !empty($qty1)) {
-			if ($size1=='satuTB') {
-				$price=50;
-			}elseif ($size1=='duaTB') {
-				$price=70;
-			}elseif ($size1=='empatTB') {
-				$price=145;
+			if ($size1=='1') {
+				$price1=50;
+			}elseif ($size1=='2') {
+				$price1=70;
+			}elseif ($size1=='4') {
+				$price1=145;
 			}	
-			$subtotal1=($price*$qty1)-(($dsc1/100)*($price*$qty1));
-			echo $subtotal1;	
+			$subtotal1=($price1*$qty1)-(($dsc1/100)*($price1*$qty1));
+			//echo $subtotal1;	
 		}elseif($item1=='seagate' && !empty($qty1)) {
-			if ($size1=='satuTB') {
-				$price=51;
-			}elseif ($size1=='duaTB') {
-				$price=73;
-			}elseif ($size1=='empatTB') {
-				$price=152;
+			if ($size1=='1') {
+				$price1=51;
+			}elseif ($size1=='2') {
+				$price1=73;
+			}elseif ($size1=='4') {
+				$price1=152;
 			}	
-			$subtotal1=($price*$qty1)-(($dsc1/100)*($price*$qty1));
-			echo $subtotal1;			
+			$subtotal1=($price1*$qty1)-(($dsc1/100)*($price1*$qty1));
+			//echo $subtotal1;			
 		}// akhir item1
 
 		if ($item2=='toshiba' && !empty($qty2)) {
-			if ($size2=='satuTB') {
-				$price=52;
-			}elseif ($size2=='duaTB') {
-				$price=75;
-			}elseif ($size2=='empatTB') {
-				$price=150;
+			if ($size2=='1') {
+				$price2=52;
+			}elseif ($size2=='2') {
+				$price2=75;
+			}elseif ($size2=='4') {
+				$price2=150;
 			}	
-			$subtotal2=($price*$qty2)-(($dsc2/100)*($price*$qty2));
-			echo $subtotal2;			
+			$subtotal2=($price2*$qty2)-(($dsc2/100)*($price2*$qty2));
+			//echo $subtotal2;			
 		}elseif($item2=='samsung' && !empty($qty2)) {
-			if ($size2=='satuTB') {
-				$price=50;
-			}elseif ($size2=='duaTB') {
-				$price=70;
-			}elseif ($size2=='empatTB') {
-				$price=145;
+			if ($size2=='1') {
+				$price2=50;
+			}elseif ($size2=='2') {
+				$price2=70;
+			}elseif ($size2=='4') {
+				$price2=145;
 			}	
-			$subtotal2=($price*$qty2)-(($dsc2/100)*($price*$qty2));
-			echo $price;
-			echo "\nprice x qty:" .$price*$qty2;
+			$subtotal2=($price2*$qty2)-(($dsc2/100)*($price2*$qty2));
+			//echo $price;
+			//echo "\nprice x qty:" .$price*$qty2;
 
-			echo "\nqty" .$qty2;
-			echo "\ndiscon:" .$dsc2;
-			echo "\n" .$subtotal2;				
+			//echo "\nqty" .$qty2;
+			//echo "\ndiscon:" .$dsc2;
+			//echo "\n" .$subtotal2;				
 		}elseif($item2=='seagate' && !empty($qty2)) {
-			if ($size2=='satuTB') {
-				$price=51;
-			}elseif ($size2=='duaTB') {
-				$price=73;
-			}elseif ($size2=='empatTB') {
-				$price=152;
+			if ($size2=='1') {
+				$price2=51;
+			}elseif ($size2=='2') {
+				$price2=73;
+			}elseif ($size2=='4') {
+				$price2=152;
 			}	
-			$subtotal2=($price*$qty2)-(($dsc2/100)*($price*$qty2));
-			echo $subtotal2;		
+			$subtotal2=($price2*$qty2)-(($dsc2/100)*($price2*$qty2));
+			//echo $subtotal2;		
 		}//akhir item2
 
 		if ($item3=='toshiba' && !empty($qty3)) {
-			if ($size3=='satuTB') {
-				$price=52;
-			}elseif ($size3=='duaTB') {
-				$price=75;
-			}elseif ($size3=='empatTB') {
-				$price=150;
+			if ($size3=='1') {
+				$price3=52;
+			}elseif ($size3=='2') {
+				$price3=75;
+			}elseif ($size3=='4') {
+				$price3=150;
 			}	
-			$subtotal3=($price*$qty3)-(($dsc3/100)*($price*$qty3));
-			echo $price;
-			echo "\nprice x qty:" .$price*$qty3;
+			$subtotal3=($price3*$qty3)-(($dsc3/100)*($price3*$qty3));
+			//echo $price;
+			//echo "\nprice x qty:" .$price*$qty3;
 
-			echo "\nqty" .$qty3;
-			echo "\ndiscon:" .$dsc3;
-			echo "\n" .$subtotal3;		
+			//echo "\nqty" .$qty3;
+			//echo "\ndiscon:" .$dsc3;
+			//echo "\n" .$subtotal3;		
 		}elseif($item3=='samsung' && !empty($qty3)) {
-			if ($size3=='satuTB') {
-				$price=50;
-			}elseif ($size3=='duaTB') {
-				$price=70;
-			}elseif ($size3=='empatTB') {
-				$price=145;
+			if ($size3=='1') {
+				$price3=50;
+			}elseif ($size3=='2') {
+				$price3=70;
+			}elseif ($size3=='4') {
+				$price3=145;
 			}	
-			$subtotal3=($price*$qty3)-(($dsc3/100)*($price*$qty3));
-			echo $price;
-			echo "\nprice x qty:" .$price*$qty3;
+			$subtotal3=($price3*$qty3)-(($dsc3/100)*($price3*$qty3));
+			//echo $price;
+			//echo "\nprice x qty:" .$price*$qty3;
 
-			echo "\nqty" .$qty3;
-			echo "\ndiscon:" .$dsc3;
-			echo "\n" .$subtotal3;			
+			//echo "\nqty" .$qty3;
+			//echo "\ndiscon:" .$dsc3;
+			//echo "\n" .$subtotal3;			
 		}elseif($item3=='seagate' && !empty($qty3)) {
-			if ($size3=='satuTB') {
-				$price=51;
-			}elseif ($size3=='duaTB') {
-				$price=73;
-			}elseif ($size3=='empatTB') {
-				$price=152;
+			if ($size3=='1') {
+				$price3=51;
+			}elseif ($size3=='2') {
+				$price3=73;
+			}elseif ($size3=='4') {
+				$price3=152;
 			}		
-			$subtotal3=($price*$qty3)-(($dsc3/100)*($price*$qty3));
-			echo $subtotal3;	
+			$subtotal3=($price3*$qty3)-(($dsc3/100)*($price3*$qty3));
+			//echo $subtotal3;	
 		}//akhir item3
 		$total=$subtotal1+$subtotal2+$subtotal3;
 		$pajak=$total*(10/100);
 		$bayar=$total+$pajak;
-		echo "Total:".$total;
-		echo "Pajak:".$pajak;
-		echo "Bayar:".$bayar;
+		//echo "Total:".$total;
+		//echo "Pajak:".$pajak;
+		//echo "Bayar:".$bayar;
 
-		
+		//tulis kefile
+		$fh = fopen("struk.txt","w");
+
+		if(!$fh){
+			die("File tidak dapat dibuka");
+		}
+
+		fwrite($fh, str_pad('BL Store', 50, " ", STR_PAD_BOTH));
+		fwrite($fh, "\n".str_pad('1911601100_Satria_Ardi_Perdana', 50, " ", STR_PAD_BOTH));
+		fwrite($fh, "\n".str_pad('+==========+=====+=====+=======+=====+==========+', 100, " "));
+		fwrite($fh, "\n".str_pad('|   Item   |Size | Qty | Price | Dsc | SubTotal |', 100, " "));
+		fwrite($fh, "\n".str_pad('+==========+=====+=====+=======+=====+==========+', 100, " "));
+		if(!empty($qty1)){
+		 fwrite($fh, "\n".str_pad('|'.$item1.'   | '.$size1.'TB |  '.$qty1.'  | '.$price1.'   | '.$dsc1.'   |  '.number_format($subtotal1, 2).'    |', 100, " "));
+		}
+		if(!empty($qty2)){
+		 fwrite($fh, "\n".str_pad('|'.$item2.'   | '.$size2.'TB |  '.$qty2.'  | '.$price2.'   | '.$dsc2.'   |  '.number_format($subtotal2, 2).'    |', 100, " "));
+		}
+		if(!empty($qty3)){
+		 fwrite($fh, "\n".str_pad('|'.$item3.'   | '.$size3.'TB |  '.$qty3.'  | '.$price3.'   | '.$dsc3.'   |  '.number_format($subtotal3, 2).'    |', 100, " "));
+		}
+		fwrite($fh, "\n".str_pad('+==========+=====+=====+=======+=====+==========+', 100, " "));
+		fwrite($fh, "\n".str_pad('Total: '.number_format($total, 2).' ', 100, " "));
+		fwrite($fh, "\n".str_pad('Pajak: '.number_format($pajak, 2).' ', 100, " "));
+		fwrite($fh, "\n".str_pad('Bayar: '.number_format($bayar, 2).' ', 100, " "));
+		//fwrite($fh, str_pad('12346789123', 12, " "));
+
+		fclose($fh);
 		
 	}//akhir isset($_POST['submit'])
 
